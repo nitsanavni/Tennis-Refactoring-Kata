6e0d6aa940c91f05b4b851f36814eab0fe383404
Typist <typist@example.com>
2023-04-13 06:13:05 +0000

    tennis 4

--- csharp/Tennis/TennisGame4.cs
+++ csharp/Tennis/TennisGame4.cs
@@ -36,33 +36,35 @@ namespace Tennis
                 p2--;
             }
 
-            if (p1 == 0 && p2 == 0) return "Love-All";
-            if (p1 == 0 && p2 == 1) return "Love-Fifteen";
-            if (p1 == 0 && p2 == 2) return "Love-Thirty";
-            if (p1 == 0 && p2 == 3) return "Love-Forty";
-            if (p1 == 0 && p2 == 4) return $"Win for {player2Name}";
-            if (p1 == 1 && p2 == 0) return "Fifteen-Love";
-            if (p1 == 1 && p2 == 1) return "Fifteen-All";
-            if (p1 == 1 && p2 == 2) return "Fifteen-Thirty";
-            if (p1 == 1 && p2 == 3) return "Fifteen-Forty";
-            if (p1 == 1 && p2 == 4) return $"Win for {player2Name}";
-            if (p1 == 2 && p2 == 0) return "Thirty-Love";
-            if (p1 == 2 && p2 == 1) return "Thirty-Fifteen";
-            if (p1 == 2 && p2 == 2) return "Thirty-All";
-            if (p1 == 2 && p2 == 3) return "Thirty-Forty";
-            if (p1 == 2 && p2 == 4) return $"Win for {player2Name}";
-            if (p1 == 3 && p2 == 0) return "Forty-Love";
-            if (p1 == 3 && p2 == 1) return "Forty-Fifteen";
-            if (p1 == 3 && p2 == 2) return "Forty-Thirty";
-            if (p1 == 3 && p2 == 3) return "Deuce";
-            if (p1 == 3 && p2 == 4) return $"Advantage {player2Name}";
-            if (p1 == 4 && p2 == 0) return $"Win for {player1Name}";
-            if (p1 == 4 && p2 == 1) return $"Win for {player1Name}";
-            if (p1 == 4 && p2 == 2) return $"Win for {player1Name}";
-            if (p1 == 4 && p2 == 3) return $"Advantage {player1Name}";
-            if (p1 == 4 && p2 == 4) return "Deuce";
-            
-            throw new ArgumentException("Invalid score.");
+            return (p1, p2) switch
+            {
+                (0, 0) => "Love-All",
+                (0, 1) => "Love-Fifteen",
+                (0, 2) => "Love-Thirty",
+                (0, 3) => "Love-Forty",
+                (0, 4) => $"Win for {player2Name}",
+                (1, 0) => "Fifteen-Love",
+                (1, 1) => "Fifteen-All",
+                (1, 2) => "Fifteen-Thirty",
+                (1, 3) => "Fifteen-Forty",
+                (1, 4) => $"Win for {player2Name}",
+                (2, 0) => "Thirty-Love",
+                (2, 1) => "Thirty-Fifteen",
+                (2, 2) => "Thirty-All",
+                (2, 3) => "Thirty-Forty",
+                (2, 4) => $"Win for {player2Name}",
+                (3, 0) => "Forty-Love",
+                (3, 1) => "Forty-Fifteen",
+                (3, 2) => "Forty-Thirty",
+                (3, 3) => "Deuce",
+                (3, 4) => $"Advantage {player2Name}",
+                (4, 0) => $"Win for {player1Name}",
+                (4, 1) => $"Win for {player1Name}",
+                (4, 2) => $"Win for {player1Name}",
+                (4, 3) => $"Advantage {player1Name}",
+                (4, 4) => "Deuce",
+                _ => throw new ArgumentException("Invalid score.")
+            };
         }
     }
 }
